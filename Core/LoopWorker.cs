6c291bd6ac4cfc96b87ed0993d36baf296c07814
Kazze <kaze47@gmail.com>
2025-03-28 09:23:49 +0100

    LoopWorker

---
+++ Core/LoopWorker.cs
@@ -0,0 +1,32 @@
+﻿namespace KazDev.Core
+{
+    public abstract class LoopWorker
+    {
+        public abstract void OnStart();
+        public abstract void OnStop();
+        public abstract void Loop();
+
+        bool isRunning = false;
+
+        public void Start()
+        {
+            if (isRunning) return;
+            isRunning = true;
+            OnStart();
+            Thread thread = new Thread(ThreadLoop) { Name = "LoopWorker" };
+            thread.Start();
+        }
+        public void Stop()
+        {
+            if (!isRunning) return;
+            isRunning = false;
+            OnStop();
+        }
+
+        void ThreadLoop()
+        {
+            while (isRunning)
+                Loop();
+        }
+    }
+}
